d529221339a3614a89783532581921f9a9abb1ef
grovince <grovinceln@gmail.com>
2023-11-02 15:35:37 +0900

    프로젝트 설정

---
+++ src/test/java/com/project/PinReterest/PinReterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PinReterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinReterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
